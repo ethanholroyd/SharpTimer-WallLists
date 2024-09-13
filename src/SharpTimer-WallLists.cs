9f11286c61f2b8444147a3bb2cc6b622df15a06b
M-archand <13469995+M-archand@users.noreply.github.com>
2024-09-13 11:26:44 -0400

    Remove plugin config

--- src/SharpTimer-WallLists.cs
+++ src/SharpTimer-WallLists.cs
@@ -11,7 +11,6 @@ using K4WorldTextSharedAPI;
 using System.Drawing;
 using System.Data.SQLite;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Dapper;
 using Microsoft.Extensions.Logging;
 using MySqlConnector;
@@ -19,101 +18,6 @@ using Npgsql;
 
 namespace SharpTimerWallLists
 {
-    public class PluginConfig : BasePluginConfig
-    {
-        [JsonPropertyName("TopCount")]
-        public int TopCount { get; set; } = 5;
-
-        [JsonPropertyName("TimeBasedUpdate")]
-        public bool TimeBasedUpdate { get; set; } = false;
-
-        [JsonPropertyName("UpdateInterval")]
-        public int UpdateInterval { get; set; } = 60;
-
-        [JsonPropertyName("DatabaseType")]
-        public int DatabaseType { get; set; } = 1; // 1 = MySQL, 2 = SQLite. 3 = Postgres
-
-        [JsonPropertyName("DatabaseSettings")]
-        public DatabaseSettings DatabaseSettings { get; set; } = new DatabaseSettings();
-
-        [JsonPropertyName("PointsTitleText")]
-        public string PointsTitleText { get; set; } = "|--- Points Leaders ---|";
-
-        [JsonPropertyName("MapsTitleText")]
-        public string MapsTitleText { get; set; } = "|---- Map Records ----|";
-
-        [JsonPropertyName("TitleFontSize")]
-        public int TitleFontSize { get; set; } = 26;
-
-        [JsonPropertyName("TitleTextScale")]
-        public float TitleTextScale { get; set; } = 0.45f;
-
-        [JsonPropertyName("ListFontSize")]
-        public int ListFontSize { get; set; } = 24;
-
-        [JsonPropertyName("ListTextScale")]
-        public float ListTextScale { get; set; } = 0.35f;
-
-        [JsonPropertyName("MaxNameLength")]
-        public int MaxNameLength { get; set; } = 32; // Default value, 32 is max Steam name length
-
-        [JsonPropertyName("TextAlignment")]
-        public string TextAlignment { get; set; } = "center";
-
-        [JsonPropertyName("TitleTextColor")]
-        public string TitleTextColor { get; set; } = "Magenta";
-
-        [JsonPropertyName("FirstPlaceColor")]
-        public string FirstPlaceColor { get; set; } = "Lime";
-
-        [JsonPropertyName("SecondPlaceColor")]
-        public string SecondPlaceColor { get; set; } = "Coral";
-
-        [JsonPropertyName("ThirdPlaceColor")]
-        public string ThirdPlaceColor { get; set; } = "Cyan";
-
-        [JsonPropertyName("DefaultColor")]
-        public string DefaultColor { get; set; } = "White";
-
-        [JsonPropertyName("PointsListCommand")]
-        public string PointsListCommand { get; set; } = "pointslist";
-
-        [JsonPropertyName("TimesListCommand")]
-        public string TimesListCommand { get; set; } = "timeslist";
-
-        [JsonPropertyName("RemoveListCommand")]
-        public string RemoveListCommand { get; set; } = "removelist";
-        
-        [JsonPropertyName("CommandPermission")]
-        public string CommandPermission { get; set; } = "@css/root";
-
-        [JsonPropertyName("ConfigVersion")]
-        public override int Version { get; set; } = 2;
-    }
-
-    public sealed class DatabaseSettings
-    {
-        [JsonPropertyName("host")]
-        public string Host { get; set; } = "localhost";
-
-        [JsonPropertyName("username")]
-        public string Username { get; set; } = "root";
-
-        [JsonPropertyName("database")]
-        public string Database { get; set; } = "database";
-
-        [JsonPropertyName("password")]
-        public string Password { get; set; } = "password";
-
-        [JsonPropertyName("port")]
-        public int Port { get; set; } = 3306;
-
-        [JsonPropertyName("sslmode")]
-        public string Sslmode { get; set; } = "none";
-        [JsonPropertyName("table-prefix")]
-	    public string TablePrefix { get; set; } = "";
-    }
-
     [MinimumApiVersion(205)]
     public class PluginSharpTimerWallLists : BasePlugin, IPluginConfig<PluginConfig>
     {
@@ -122,9 +26,7 @@ namespace SharpTimerWallLists
         public override string ModuleVersion => "1.0.1";
 
         public required PluginConfig Config { get; set; } = new PluginConfig();
-
         public static PluginCapability<IK4WorldTextSharedAPI> Capability_SharedAPI { get; } = new("k4-worldtext:sharedapi");
-
         private List<int> _currentPointsList = new();
         private List<int> _currentMapList = new();
         private CounterStrikeSharp.API.Modules.Timers.Timer? _updateTimer;
@@ -135,9 +37,9 @@ namespace SharpTimerWallLists
         public void OnConfigParsed(PluginConfig config)
         {
             if (config.Version < Config.Version)
-                base.Logger.LogWarning("Configuration version mismatch (Expected: {0} | Current: {1})", this.Config.Version, config.Version);
+                Logger.LogWarning("Configuration version mismatch (Expected: {0} | Current: {1})", Config.Version, config.Version);
 
-            this.Config = config;
+            Config = config;
         }
 
         public override void OnAllPluginsLoaded(bool hotReload)
@@ -256,7 +158,6 @@ namespace SharpTimerWallLists
                 Server.NextWorldUpdate(() => RemoveClosestList(player, command));
             });
         }
-
 
         private void CreateTopList(CCSPlayerController player, CommandInfo command, ListType listType)
         {
@@ -383,7 +284,6 @@ namespace SharpTimerWallLists
                 Logger.LogError(ex, "Error removing list in RemoveClosestList method.");
             }
         }
-
 
         private float DistanceTo(Vector a, Vector b)
         {
